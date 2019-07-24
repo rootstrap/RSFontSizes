98d635f5d4705caf1c769da84d373eab7581ede5
German Lopez <german@rootstrap.com>
2019-07-24 15:28:13 -0300

    Fix pod linting issues

--- RSFontSizes/Classes/UIFontExtensions.swift
+++ RSFontSizes/Classes/UIFontExtensions.swift
@@ -47,51 +47,51 @@ public extension UIFont {
   
   // MARK: Weight Functions
   
-  public var ultraLight: UIFont { return weight(.ultraLight) }
-  public var extraLight: UIFont { return weight(.extraLight) }
-  public var thin: UIFont { return weight(.thin) }
-  public var book: UIFont { return weight(.book) }
-  public var demi: UIFont { return weight(.demi) }
-  public var normal: UIFont { return weight(.normal) }
-  public var light: UIFont { return weight(.light) }
-  public var regular: UIFont { return weight(.regular) }
-  public var medium: UIFont { return weight(.medium) }
-  public var demibold: UIFont { return weight(.demibold) }
-  public var semibold: UIFont { return weight(.semibold) }
-  public var bold: UIFont { return weight(.bold) }
-  public var extraBold: UIFont { return weight(.extraBold) }
-  public var heavy: UIFont { return weight(.heavy) }
-  public var black: UIFont { return weight(.black) }
-  public var extraBlack: UIFont { return weight(.extraBlack) }
-  public var ultraBlack: UIFont { return weight(.ultraBlack) }
-  public var fat: UIFont { return weight(.fat) }
-  public var poster: UIFont { return weight(.poster) }
+  var ultraLight: UIFont { return weight(.ultraLight) }
+  var extraLight: UIFont { return weight(.extraLight) }
+  var thin: UIFont { return weight(.thin) }
+  var book: UIFont { return weight(.book) }
+  var demi: UIFont { return weight(.demi) }
+  var normal: UIFont { return weight(.normal) }
+  var light: UIFont { return weight(.light) }
+  var regular: UIFont { return weight(.regular) }
+  var medium: UIFont { return weight(.medium) }
+  var demibold: UIFont { return weight(.demibold) }
+  var semibold: UIFont { return weight(.semibold) }
+  var bold: UIFont { return weight(.bold) }
+  var extraBold: UIFont { return weight(.extraBold) }
+  var heavy: UIFont { return weight(.heavy) }
+  var black: UIFont { return weight(.black) }
+  var extraBlack: UIFont { return weight(.extraBlack) }
+  var ultraBlack: UIFont { return weight(.ultraBlack) }
+  var fat: UIFont { return weight(.fat) }
+  var poster: UIFont { return weight(.poster) }
   
   // MARK: Size Functions
-  public var tiny: UIFont { return sized(.tiny) }
-  public var small: UIFont { return sized(.small) }
-  public var normalSize: UIFont { return sized(.normal) }
-  public var mediumSize: UIFont { return sized(.medium) }
-  public var big: UIFont { return sized(.big) }
-  public var huge: UIFont { return sized(.huge) }
-  public var enormous: UIFont { return sized(.enormous) }
-  public func fixed(_ points: CGFloat) -> UIFont { return sized(.fixed(points: points)) }
-  public func specific(values: SizeSpecification) -> UIFont { return sized(.specific(with: values)) }
-  public func proportional(to base: BaseSize) -> UIFont { return sized(.proportional(to: base)) }
+  var tiny: UIFont { return sized(.tiny) }
+  var small: UIFont { return sized(.small) }
+  var normalSize: UIFont { return sized(.normal) }
+  var mediumSize: UIFont { return sized(.medium) }
+  var big: UIFont { return sized(.big) }
+  var huge: UIFont { return sized(.huge) }
+  var enormous: UIFont { return sized(.enormous) }
+  func fixed(_ points: CGFloat) -> UIFont { return sized(.fixed(points: points)) }
+  func specific(values: SizeSpecification) -> UIFont { return sized(.specific(with: values)) }
+  func proportional(to base: BaseSize) -> UIFont { return sized(.proportional(to: base)) }
 }
 
 public extension UIFont.Weight {
   private static let equatableDiff: CGFloat = 0.0001
-  public static let extraLight = UIFont.Weight(rawValue: UIFont.Weight.ultraLight.rawValue + equatableDiff)
-  public static let book = UIFont.Weight(rawValue: -0.5)
-  public static let demi = UIFont.Weight(rawValue: -0.5 + equatableDiff)
-  public static let normal = UIFont.Weight(rawValue: 0 + equatableDiff)
-  public static let demibold = UIFont.Weight(rawValue: UIFont.Weight.semibold.rawValue + equatableDiff)
-  public static let extraBold = UIFont.Weight(rawValue: UIFont.Weight.black.rawValue + equatableDiff)
-  public static let extraBlack = UIFont.Weight(rawValue: 0.8)
-  public static let ultraBlack = UIFont.Weight(rawValue: UIFont.Weight.extraBlack.rawValue + equatableDiff)
-  public static let fat = UIFont.Weight(rawValue: UIFont.Weight.ultraBlack.rawValue + equatableDiff)
-  public static let poster = UIFont.Weight(rawValue: UIFont.Weight.fat.rawValue + equatableDiff)
+  static let extraLight = UIFont.Weight(rawValue: UIFont.Weight.ultraLight.rawValue + equatableDiff)
+  static let book = UIFont.Weight(rawValue: -0.5)
+  static let demi = UIFont.Weight(rawValue: -0.5 + equatableDiff)
+  static let normal = UIFont.Weight(rawValue: 0 + equatableDiff)
+  static let demibold = UIFont.Weight(rawValue: UIFont.Weight.semibold.rawValue + equatableDiff)
+  static let extraBold = UIFont.Weight(rawValue: UIFont.Weight.black.rawValue + equatableDiff)
+  static let extraBlack = UIFont.Weight(rawValue: 0.8)
+  static let ultraBlack = UIFont.Weight(rawValue: UIFont.Weight.extraBlack.rawValue + equatableDiff)
+  static let fat = UIFont.Weight(rawValue: UIFont.Weight.ultraBlack.rawValue + equatableDiff)
+  static let poster = UIFont.Weight(rawValue: UIFont.Weight.fat.rawValue + equatableDiff)
   
   var faceName: String {
     switch self {
